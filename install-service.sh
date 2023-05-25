8e3ca2ae2f726ff10976513518391b7023194adf
nahiyan-DevOps <nahiyan.mubashshir@bjitacademy.com>
2023-05-25 19:45:16 +0600

    service added

--- install-service.sh
+++ install-service.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Copy project.service unit file to system folder
-sudo cp /opt/programming_planet/shell_files/app.service /etc/systemd/system/
+sudo cp /opt/programming_planet/app.service /etc/systemd/system/
 #sudo cp /var/misc-ml/bulk_mail_service/shell/api.service /etc/systemd/system/
 # Load ALL system unit files into memory
 sudo systemctl daemon-reload
 # Make the service start during boot [without extension enable]
+sudo systemctl start app
 sudo systemctl enable app
 #sudo systemctl enable api
