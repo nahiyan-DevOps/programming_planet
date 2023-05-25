df80f92fb371fdb387de5b4ab2b84e25ddfed1d0
nahiyan-DevOps <nahiyan.mubashshir@bjitacademy.com>
2023-05-25 19:07:55 +0600

    service added

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt install python3-pip
+echo "python3-pip installed"
+sudo apt install python3-virtualenv
+echo "python3-virtualenv installed"
+virtualenv /opt/programming_planet/venv
+echo "venv created"
+source /opt/programming_planet/venv/bin/activate
+echo "venv activated"
+pip3 install -r /opt/programming_planet/requirements.txt
+echo "requirements installed"
+deactivate
