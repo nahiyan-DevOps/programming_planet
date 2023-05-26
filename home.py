631eb898fad69e38b25e2abefdba8261b74fb714
nahiyan-DevOps <nahiyan.mubashshir@bjitacademy.com>
2023-05-26 12:04:22 +0600

    db connection added

--- home.py
+++ home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import mysql.connector
+import pandas as pd
 
 def home():
     logo_col1, logo_col2, logo_col3 = st.columns(3)
@@ -20,3 +22,45 @@ def home():
         st.image("images/python.png", width=150)
 
     st.subheader("Programming Planet is a place where every newbie can get a head start to the magical world of coding")
+    st.empty()
+    st.subheader("About Me")
+    # MySQL database connection details
+    host = '192.168.20.112'
+    port = 3306
+    user = 'app_db'
+    password = '1234'
+    db_name = 'app_db'
+    table_name = 'about'
+
+    # Connect to the MySQL database
+    try:
+        connection = mysql.connector.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        cursor = connection.cursor()
+
+        # Fetch all rows from the table
+        query = f"SELECT * FROM {table_name}"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        # Create a pandas DataFrame from the fetched data
+        df = pd.DataFrame(rows, columns=[desc[0] for desc in cursor.description])
+        df = df.reset_index(drop=True)
+        df = df.drop('id', axis=1)
+        df = df.set_index('author_name')
+        # Display the DataFrame in Streamlit
+        st.dataframe(df)
+
+    except mysql.connector.Error as error:
+        st.error(f"Error connecting to MySQL: {error}")
+
+    finally:
+        # Close the database connection
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
